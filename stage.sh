86ee86eb71c609a8cb03b5f23230c0bd08a23385
Jermolene <jeremy@osmosoft.com>
2014-09-18 12:11:31 +0100

    Fix staging script

--- stage.sh
+++ stage.sh
@@ -3,6 +3,7 @@
 # stage TalkyTalky editions for tiddlywiki.com/talkytalky by copying them into the jermolene.github.com repo
 
 mkdir -p ../jermolene.github.com/talkytalky/lnug
+mkdir -p ../jermolene.github.com/talkytalky/innoq
 
 cp -R 2014-04\ LNUG/output/* ../jermolene.github.com/talkytalky/lnug
 cp -R 2014-06\ InnoQ/output/* ../jermolene.github.com/talkytalky/innoq
